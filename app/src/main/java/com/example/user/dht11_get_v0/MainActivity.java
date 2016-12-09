816eb9d09964bc8c610955244ec70b12823060f0
albert <alber@gmail.com>
2016-12-09 10:14:23 +0800

    增加auto update data功能 (Thread & Handler)

--- app/src/main/java/com/example/user/dht11_get_v0/MainActivity.java
+++ app/src/main/java/com/example/user/dht11_get_v0/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.user.dht11_get_v0;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +13,8 @@ import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +29,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import static android.R.attr.data;
+import static com.android.volley.toolbox.Volley.newRequestQueue;
 
 //-------------------- 設定變數 --------------------------------------------------
 class Variable {
@@ -35,17 +40,18 @@ class Variable {
     static String titlePm25 = titleMain + " -- PM2.5資訊";
 
     static int webScale = 150;                       // set webview scale
-    static int fieldResults = 100;                    // results=? 設定抓取筆數
+    static int fieldResults = 100 ;                    // results=? 設定抓取筆數
     static String api_key = "TC1HMH4R7SSVE93A";      // ThingSpeak API key
     // last資料url
     static String urlLast = "https://api.thingspeak.com/channels/189185/feed/last.json?";
-     //static String urlLast = "https://api.thingspeak.com/channels/176126/feed/last.json?";   // for test url
+     //static String urlLast = "https://api.thingspeak.com/channels/176126/feed/last.json?";   // url for testing
      // 圖表資料url
     static String urlTem = "https://api.thingspeak.com/channels/189185/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&type=line&timezone=Asia/Taipei&results=" + fieldResults;
     static String urlHum = "https://api.thingspeak.com/channels/189185/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&type=line&timezone=Asia/Taipei&results=" + fieldResults;
     static String urlPm25 = "https://api.thingspeak.com/channels/189185/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&type=line&timezone=Asia/Taipei&results=" + fieldResults;
      // all資料url
     static String urlFeeds = "https://api.thingspeak.com/channels/189185/feeds.json?&results=" + fieldResults;
+    //static final int msgKey1 = 111;
 
 }
 //------------------------------------------------------------------------------------
@@ -53,6 +59,8 @@ class Variable {
 public class MainActivity extends AppCompatActivity {
 
     TextView tv1, tv2, tv3;
+    private static final int msgKey1 = 111;
+
 
 //    Button btnTem;
 //    Button btnHum;
@@ -68,10 +76,46 @@ public class MainActivity extends AppCompatActivity {
         tv2 = (TextView) findViewById(R.id.textView2);      // hum
         tv3 = (TextView) findViewById(R.id.textView3);      // pm2.5
 
+        getNowData();
+        new updateThread().start();
     }
-    // ----------- 抓取目前環境資訊(ThinkSpeak last data) -------------------------------
-    public void clickNow(View v) {
-        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+    // ---------- auto update data (Thread & Handler) --------------------------------------
+    public class updateThread extends Thread {
+        @Override
+        public void run () {
+            do{
+                try {
+                    Thread.sleep(30000);
+                    Message msg = new Message();
+                    msg.what = msgKey1;
+                    mHandler.sendMessage(msg);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }while(true);
+        }
+    }
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage (Message msg) {
+            super.handleMessage(msg);
+            //Log.d("NEW",String.valueOf(msg.what));
+            if (msg.what == msgKey1)
+            {
+                getNowData();
+                Log.d("NEW","Main");
+            }
+        }
+    };
+    // -------------------------------------------------------------------
+
+    // ******* HTTP GET 抓取目前環境資訊(ThinkSpeak last data) ****************************
+    public void getNowData() {
+
+        RequestQueue mQueue = Volley.newRequestQueue(MainActivity.this);
         // feed需利用Object解析
         StringRequest request = new StringRequest(Variable.urlLast,   // + "&api_key=" + Variable.api_key,
 
@@ -104,10 +148,17 @@ public class MainActivity extends AppCompatActivity {
         }
         );
 
-        queue.add(request);
-        queue.start();
+        mQueue.add(request);
+        mQueue.start();
+
     }
-    //-------------------------------------------------------------------------------------
+    //********************************************************************************
+
+
+    // -------- call getNowData() -------------------------------
+    public void clickNow(View v) {
+        getNowData();
+    }
 
     // ----- Button for getting chart data ----------------------
     public void clickTemp(View view) {
@@ -131,6 +182,59 @@ public class MainActivity extends AppCompatActivity {
 
 }
 
+/*
+// try thread
+class NowData extends Thread
+{
+    static TextView tv1, tv2, tv3;
+
+    public RequestQueue mQueue = newRequestQueue(MainActivity);
+    // feed需利用Object解析
+    StringRequest request = new StringRequest(Variable.urlLast,   // + "&api_key=" + Variable.api_key,
+
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+
+//                    public static void run(String response)
+//    {
+        // ------- JSON 寫法 -------
+        try
+        {
+            Thread.sleep(10000);
+            JSONObject objLast = new JSONObject(response);
+            String tempLast = objLast.getString("field1");
+            String humiLast = objLast.getString("field2");
+            String pm25Last = objLast.getString("field3");
+
+            tv1.setText("目前溫度值:     " + tempLast + " \u2103");  //℃ Unicode
+            tv2.setText("目前濕度值:     " + humiLast + " %RH");
+            tv3.setText("目前PM2.5濃度:  " + pm25Last + " μg/m3");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // --------------------------
+//    }
+
+                }
+            }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+
+        }
+    }
+    );
+
+    mQueue.add(request);
+    mQueue.start();
+
+
+}
+*/
 
 // -------- setOnClickListener method -------------------------------------
 // Button setOnClickListener
